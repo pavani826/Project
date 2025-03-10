bd473d8b5e7e21531474d6c1d1450787ab1bc1a9
Pavani <pavani826>
2025-03-10 18:45:33 +0545

    First

---
+++ include.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Include Example</h1>
+    <?php include 'values.php';
+    echo "I have a $color $car car"."<br>";
+    echo "The price of the car is $price";
+    ?>
+
+</body>
+</html>
